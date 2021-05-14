0eb2fe396f87c5e7847d2967dab66d12894f94b6
JorisAlbers <joris_albers@hotmail.com>
2021-05-14 11:12:38 +0200

    Get list of connected cameras

---
+++ CloudCam/CameraEnumerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DirectShowLib;
+
+namespace CloudCam
+{
+    public record CameraDevice(string DeviceId, string Name, int OpenCvId);
+
+    public static class CameraDevicesEnumerator
+    {
+        public static List<CameraDevice> GetAllConnectedCameras()
+        {
+            var videoInputDevices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+            int openCvId = 0;
+            return videoInputDevices.Select(v => new CameraDevice(v.DevicePath, v.Name, openCvId++)).ToList();
+        }
+    }
+}
