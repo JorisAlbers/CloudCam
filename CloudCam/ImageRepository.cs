fc1417add9e4f5f41168c3e81ba4b6e901a5a108
JorisAlbers <joris_albers@hotmail.com>
2021-05-15 15:59:22 +0200

    Added ImageRepository

---
+++ CloudCam/ImageRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace CloudCam
+{
+    public class ImageRepository
+    {
+        private readonly string _folderPath;
+        private string[] _images;
+
+        public int Count => _images.Length;
+
+        public ImageRepository(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+
+        public void Load()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(_folderPath);
+            _images = directoryInfo.EnumerateFiles("*.png").Select(x => x.Name).ToArray();
+        }
+
+        public Mat this[int index] => Cv2.ImRead(Path.Combine(_folderPath, _images[index]));
+    }
+}
