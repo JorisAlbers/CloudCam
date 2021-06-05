c916e5b44c977ad75d9386b5c31c54a98a7d1e96
JorisAlbers <joris_albers@hotmail.com>
2021-06-05 12:10:07 +0200

    Added FaceDetection

---
+++ CloudCam/Detection/FaceDetection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace CloudCam.Detection
+{
+    public class FaceDetection
+    {
+        private readonly CascadeClassifier _cascade;
+
+        public FaceDetection(string faceCascadeFile)
+        {
+            _cascade = new CascadeClassifier(faceCascadeFile);
+        }
+
+        public Rect[] Detect(Mat image)
+        {
+            var grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGRA2GRAY);
+            Cv2.EqualizeHist(grayImage, grayImage);
+
+            return _cascade.DetectMultiScale(
+                image: grayImage,
+                scaleFactor: 1.1,
+                minNeighbors: 2,
+                flags: HaarDetectionTypes.DoRoughSearch | HaarDetectionTypes.ScaleImage,
+                minSize: new Size(30, 30)
+            );
+        }
+    }
+}
