9b9ed1447aad64732f90c9afb915eb03965819f3
JorisAlbers <joris_albers@hotmail.com>
2021-06-05 12:13:20 +0200

    Added NoseDetection

---
+++ CloudCam/Detection/NoseDetection.cs
@@ -0,0 +1,27 @@
+﻿using OpenCvSharp;
+
+namespace CloudCam.Detection
+{
+    public class NoseDetection
+    {
+        private readonly CascadeClassifier _cascade;
+
+        public NoseDetection(string cascadeFile)
+        {
+            _cascade = new CascadeClassifier(cascadeFile);
+        }
+
+        public Rect[] Detect(Mat image)
+        {
+            var grayImage = new Mat();
+            Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGRA2GRAY);
+            return _cascade.DetectMultiScale(
+                image: grayImage,
+                scaleFactor: 1.1,
+                minNeighbors: 2,
+                flags: HaarDetectionTypes.DoRoughSearch | HaarDetectionTypes.ScaleImage,
+                minSize: new Size(30, 30)
+            );
+        }
+    }
+}
