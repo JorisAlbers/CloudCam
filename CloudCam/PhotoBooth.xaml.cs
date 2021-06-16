fcc98255c53b5f1c12a8cafe79f6e77f852f6555
JorisAlbers <joris_albers@hotmail.com>
2021-06-16 20:22:00 +0200

    Show a video of clouds as background when the taken picture is shown

--- CloudCam/PhotoBooth.xaml.cs
+++ CloudCam/PhotoBooth.xaml.cs
@@ -44,14 +44,31 @@ namespace CloudCam
                 this.OneWayBind(ViewModel, vm => vm.SecondsUntilPictureIsTaken, v => v.FlashRectangle.Visibility,
                     (seconds) => seconds == 0 ? Visibility.Visible : Visibility.Hidden).DisposeWith(d);
 
-                this.OneWayBind(ViewModel, vm => vm.TakenImage, v => v.BlackRectangle.Visibility,
-                    (picture) => picture != null ? Visibility.Visible : Visibility.Hidden).DisposeWith(d);
+                this.OneWayBind(ViewModel, vm => vm.TakenImage, v => v.VideoPlayer.Visibility,
+                    (picture) =>
+                    {
+                        if (picture != null)
+                        {
+                           VideoPlayer.Play();
+                            return Visibility.Visible;
+
+                        }
+                        VideoPlayer.Pause();
+                        return Visibility.Hidden;
+                        
+                    }).DisposeWith(d);
 
                 this.OneWayBind(ViewModel, vm => vm.PickupLine, v => v.PickupLineTextBlock.Text).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.CameraFps, v => v.CameraFpsTextBlock.Text).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.EditingFps, v => v.EditingFpsTextBlock.Text).DisposeWith(d);
                 this.OneWayBind(ViewModel, vm => vm.ToDisplayImageFps, v => v.ToDisplayImageTextBlock.Text).DisposeWith(d);
             });
+        }
+
+        private void VideoPlayer_OnMediaEnded(object sender, RoutedEventArgs e)
+        {
+            VideoPlayer.Position = TimeSpan.Zero;
+            VideoPlayer.Play();
         }
     }
 }
