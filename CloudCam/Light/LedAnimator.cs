6910464a269c7e8af7ef380879610b60d63de27a
JorisAlbers <joris_albers@hotmail.com>
2021-07-05 20:00:33 +0200

    Added LedAnimator

---
+++ CloudCam/Light/LedAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Light;
+
+namespace CloudCam.Light
+{
+    public class LedAnimator
+    {
+        private readonly LedController _ledController;
+
+        public LedAnimator(LedController ledController)
+        {
+            _ledController = ledController;
+        }
+
+        public async Task StartAsync()
+        {
+            await _ledController.StartAsync();
+        }
+    }
+}
