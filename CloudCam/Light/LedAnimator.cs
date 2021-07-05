296d2b55bbaf8c3350b80b9525ed32724814f95e
JorisAlbers <joris_albers@hotmail.com>
2021-07-05 20:17:26 +0200

    Added Flash method

--- CloudCam/Light/LedAnimator.cs
+++ CloudCam/Light/LedAnimator.cs
@@ -9,10 +9,14 @@ namespace CloudCam.Light
 {
     public class LedAnimator
     {
+        private readonly int _pixelsFront;
+        private readonly int _pixelsSide;
         private readonly LedController _ledController;
 
-        public LedAnimator(LedController ledController)
+        public LedAnimator(int pixelsFront, int pixelsSide,LedController ledController)
         {
+            _pixelsFront = pixelsFront;
+            _pixelsSide = pixelsSide;
             _ledController = ledController;
         }
 
@@ -20,5 +24,18 @@ namespace CloudCam.Light
         {
             await _ledController.StartAsync();
         }
+
+        public async Task Flash(int seconds)
+        {
+            _ledController.StartAnimationAtFront(new SingleColor(_pixelsFront, RgbToInt(255,255,255)));
+            await Task.Delay(seconds * 1000);
+            _ledController.StartAnimationAtFront(null);
+        }
+
+        public static int RgbToInt(byte r, byte g, byte b)
+        {
+            return g << 16 | r << 8 | b;
+        }
+
     }
 }
