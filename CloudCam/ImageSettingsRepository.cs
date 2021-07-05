148b1ab841b498e445e61c3aa490913dbe1c5c42
JorisAlbers <joris_albers@hotmail.com>
2021-07-05 15:38:37 +0200

    Added ImageSettingsRepository

---
+++ CloudCam/ImageSettingsRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CloudCam
+{
+    public class ImageSettingsRepository
+    {
+        private string _folderPath;
+
+        public ImageSettingsRepository(string folderPath)
+        {
+            _folderPath = folderPath;
+        }
+
+        public bool TryLoad(string imagePath, out ImageSettings settings)
+        {
+            if(!File.Exists(imagePath))
+            {
+                settings = null;
+                return false;
+            }
+
+            string settingsPath = Path.ChangeExtension(imagePath, ".json");
+            if (!File.Exists(settingsPath))
+            {
+                settings = null;
+                return false;
+            }
+
+            using StreamReader reader = new StreamReader(settingsPath);
+            string data = reader.ReadToEnd();
+            settings = JsonConvert.DeserializeObject<ImageSettings>(data);
+            return true;
+        }
+    }
+
+    public class ImageSettings
+    {
+        public int X { get; }
+        public int Y { get; }
+        public float Scale { get; }
+
+        public ImageSettings(int x, int y, float scale)
+        {
+            X = x;
+            Y = y;
+            Scale = scale;
+        }
+    }
+}
