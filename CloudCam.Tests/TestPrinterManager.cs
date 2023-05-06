a947a63274cfb07503065cd6df9f9332e6a21d05
JorisAlbers <joris_albers@hotmail.com>
2023-05-06 15:29:45 +0200

    Added a skeleton for a test class, TestPrinterManager

---
+++ CloudCam.Tests/TestPrinterManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CloudCam.Printing;
+using NUnit.Framework;
+
+namespace CloudCam.Tests
+{
+    public class TestPrinterManager
+    {
+        [Test]
+        public void ATest()
+        {
+            string printerName = "myPrinter";
+            string imagePath = @"C:\testImage.png";
+            var bitmap = new Bitmap(Image.FromFile(imagePath));
+
+            var printerManager = new PrinterManager(printerName);
+            printerManager.Initialize();
+            printerManager.Print(bitmap);
+        }
+    }
+}
