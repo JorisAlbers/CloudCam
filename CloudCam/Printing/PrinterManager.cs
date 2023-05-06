3b62b45edb3a93d498ca52ebd9a66d43262da299
JorisAlbers <joris_albers@hotmail.com>
2023-05-06 15:18:54 +0200

    Added skeleton for the printerManager

---
+++ CloudCam/Printing/PrinterManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+namespace CloudCam.Printing
+{
+    public class PrinterManager
+    {
+        private readonly string _printerName;
+
+        public PrinterManager(string printerName)
+        {
+            _printerName = printerName;
+            // set properties
+        }
+        
+        public void Initialize()
+        {
+            // load stuff that might take a while
+        }
+
+        public void Print(Bitmap image)
+        {
+            // print an image
+        }
+
+        public int PrintsRemaining()
+        {
+            // return number of prints remaining
+            return -1;
+        }
+
+    }
+}
