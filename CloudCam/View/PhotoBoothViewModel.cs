3ab97fd7f4431aa568ab55e71b9a778c9a5b092b
JorisAlbers <joris_albers@hotmail.com>
2023-07-06 20:50:50 +0200

    draw the foreground on the taken image

--- CloudCam/View/PhotoBoothViewModel.cs
+++ CloudCam/View/PhotoBoothViewModel.cs
@@ -455,6 +455,9 @@ namespace CloudCam.View
             await Task.Delay(500, cancellationToken); // allow camera to adjust to the flash
 
             var imageAsBitmap = ImageSource.Mat.ToBitmap();
+            var foregrounds   = ForegroundImages;
+
+
             if (Frame?.Mat != null)
             {
                 using Bitmap frameAsBitmap = Frame.Mat.ToBitmap();
@@ -462,7 +465,19 @@ namespace CloudCam.View
                 {
                     // Overlay frame on top of image
                     using Graphics gr = Graphics.FromImage(imageAsBitmap);
+                    
+                    // Draw foregrounds (the hats and mustaches and stuff)
+                    foreach (ForegroundImage foregroundImage in foregrounds)
+                    {
+                        var resized = foregroundImage.image.Resize(foregroundImage.rect.Size);
+                        var bitmap = resized.ToBitmap();
+
+                        gr.DrawImage(bitmap, new System.Drawing.Point(foregroundImage.rect.X, foregroundImage.rect.Y));
+                    }
+                    
+                    // Draw frame
                     gr.DrawImage(frameAsBitmap, new System.Drawing.Point(0, 0));
+                    
                 }, cancellationToken);
             }
             _ledAnimator.EndFlash();
